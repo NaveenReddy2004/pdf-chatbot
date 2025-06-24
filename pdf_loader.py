24f06b52ef678320f4a7e832d1a24e179b9e7864
NaveenReddy2004 <naveen18659@gmail.com>
2025-06-24 11:27:24 +0530

    Update pdf_loader.py

--- pdf_loader.py
+++ pdf_loader.py
@@ -5,6 +5,6 @@ def extract_text_from_pdf(pdf_path):
     with pdfplumber.open(pdf_path) as pdf:
         for page in pdf.pages:
             text = page.extract_text()
-            if text:
+            if text and text.strip():  
                 full_text += text + "\n"
     return full_text
