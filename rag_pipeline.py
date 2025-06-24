d93335cb5439c952e22c056edc159f94a5d97319
NaveenReddy2004 <naveen18659@gmail.com>
2025-06-24 10:39:12 +0530

    Update rag_pipeline.py

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -16,12 +16,18 @@ def build_vector_store_from_pdf(pdf_path):
     return vector_store
 
 def query_rag_system(user_query, vector_store):
-    query_embedding = get_embedding(user_query)[0]
+    embedding_response = get_embedding(user_query)
+    if not embedding_response:
+        return "Error: Failed to generate embedding.", []
+
+    query_embedding = embedding_response[0]
     relevant_chunks = vector_store.search(query_embedding, top_k=3)
 
     context = "\n\n".join(relevant_chunks)
-    prompt = f"""You are a helpful assistant. Based on the following document content, answer the user's question:\n\n{context}\n\nQuestion: {user_query}\nAnswer:"""
+    prompt = f"""You are a professional analyser of documnets so act like helpful assistant. Based on the following document content, answer the user's question:
+    \n\n{context}\n\nQuestion: {user_query}\nAnswer:"""
 
     llm = GroqLLM()
     response = llm(prompt)
     return response, relevant_chunks
+
